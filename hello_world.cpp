84ca3ce97dc6b8704cf1333d30746e10d72aa5c2
milburn brock <96309943+atomic-badger@users.noreply.github.com>
2022-05-05 22:06:29 -0500

    Add files via upload

---
+++ hello_world.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+//THIS  IS A HELLO WORLD PROGRAM WRITTEN TO TEST PROGRAM AND SYSTEM OUTPUT
+
+int  main()
+
+{
+    std::cout << "HELLO WORLD!" << std::endl;
+}
---
+++ py_hello.py
@@ -0,0 +1,1 @@
+print("\nHELLO, WORLD!\n")
